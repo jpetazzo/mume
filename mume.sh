24c1ebc20621dabafeb04271cd3d1738aa20e012
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2018-07-06 15:10:27 -0500

    Improve startup script

--- mume.sh
+++ mume.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 export PATH=.:$PATH
-pidof mmapper || mmapper &
+[ -f .login ] || {
+	echo "#NOP if you want you can put your login and password below, on two separate lines." >.login
+}
+pidof mmapper || {
+	echo "Starting mmapper..."
+	mmapper &
+       	sleep 5
+}
 tt++ -G tintin/main.tin
